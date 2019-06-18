d03869fdbb7bf4efa59ce02cd891443263394334
Miguel Machado <miiguellmp@gmail.com>
2019-06-18 10:51:22 -0300

    creating session

--- backend/database/migrations/1503250034280_token.js
+++ backend/database/migrations/1503250034280_token.js
@@ -5,11 +5,19 @@ const Schema = use('Schema')
 
 class TokensSchema extends Schema {
   up () {
-    this.create('tokens', (table) => {
+    this.create('tokens', table => {
       table.increments()
-      table.integer('user_id').unsigned().references('id').inTable('users')
-      table.string('token', 255).notNullable().unique().index()
-      table.string('type', 80).notNullable()
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+      table
+        .string('token')
+        .notNullable()
+        .unique()
+        .index()
+      table.string('type').notNullable()
       table.boolean('is_revoked').defaultTo(false)
       table.timestamps()
     })
--- backend/database/seeds/DatabaseSeeder.js
+++ backend/database/seeds/DatabaseSeeder.js
@@ -18,7 +18,7 @@ class DatabaseSeeder {
     const user = await User.create({
       name: 'Miguel Machado',
       email: 'miiguellmp@gmail.com',
-      password: '123456'
+      password: 'Miguel@1234@2912'
     })
 
     await user.teams().create({
